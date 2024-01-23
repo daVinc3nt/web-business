767ac676c2b45c6c9e110a808194c425da76b45e
Quang Trần <quangtran.dvincent@gmail.com>
2024-01-23 23:00:56 +0700

    added pricelist

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
         },
         {
           title: intl.formatMessage({ id: 'NavBar.serv.subroute2' }),
-          url: "/userhelp",
+          url: "/pricelist",
         },
       ],
     },
